285246cd4ea8a66d133733c87aedc3b7371c897e
davisermenho <72174640+Davisermenho@users.noreply.github.com>
2026-06-10 23:41:46 -0300

    Exporta contrato goalkeeper v1

--- scoutpraia/contracts/__init__.py
+++ scoutpraia/contracts/__init__.py
@@ -1,6 +1,7 @@
 """Contracts for staged event modules not yet imported into the app."""
 
 from scoutpraia.contracts.events_v1 import (
+    GOALKEEPER_V1,
     IMPORT_RULE_V1_BLOCKED,
     MODULE_CONTRACTS_V1,
     SHOOTOUT_V1,
@@ -15,6 +16,7 @@ from scoutpraia.contracts.events_v1 import (
 )
 
 __all__ = [
+    "GOALKEEPER_V1",
     "IMPORT_RULE_V1_BLOCKED",
     "MODULE_CONTRACTS_V1",
     "SHOOTOUT_V1",
